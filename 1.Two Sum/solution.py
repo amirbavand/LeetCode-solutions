909ff603a0a73812a3bd27d93182221e1bc38691
amirbavand <amirhossein.bavand@gmail.com>
2021-02-05 22:23:31 -0500

    twosum problem added

---
+++ 1.Two Sum/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        result = []
+        value_to_index_map = {}
+
+        for i in range(0, len(nums)):
+
+            if((target-nums[i]) in value_to_index_map):
+                return[value_to_index_map[(target-nums[i])], i]
+            value_to_index_map[nums[i]] = i
+
+        print(value_to_index_map)
+
+
+a = Solution()
+print((a.twoSum([-3, 4, 3, 90], 0)))
